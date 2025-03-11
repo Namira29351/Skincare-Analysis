b9431e86eff5913ea64f06d3f54c81f3f3a0df02
Student <student@Admins-MacBook-Pro.local>
2025-03-11 17:38:05 -0400

    I continued to setup my express middleware and I set up a way to query my database fromy backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
+// We use express to create our servers endpoints, and listen for & respond to requests from the frontend
 import express from "express";
+// We use dotenv so that we can access our environment variables
 import "dotenv/config";
+// We import our index.js so that we can query our database
+import * as db from "./db/index.js";
 
 const requestHandler = express();
 
@@ -8,4 +12,8 @@ requestHandler.use(express.json());
 
 requestHandler.listen(port, () => {
     console.log(`Server listening on port ${port}`);
+  });
+
+requestHandler.get("/api/v1/get-template", (req, res) => {
+    res.send("Hello World!");
   });
